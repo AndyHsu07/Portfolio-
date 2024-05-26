68d971b42e908295880bc4e8c8e1a5bf018239a7
AndyHsu07 <a7701611@gmail>
2024-05-25 20:19:50 -0400

    Center the input boxes

--- client/src/contact.jsx
+++ client/src/contact.jsx
@@ -14,23 +14,32 @@ export default function Contact(){
     return(
     <>
                 <form action="contact" onSubmit={handleSubmit}>
+                    <div className="content">
                     <h1>Contact me</h1>
                     <p>If you are interested in my experience, feel free to reach me out.</p>
-
+                    </div>
+                    <div className="container">
                      <label htmlFor="name">Name:</label>
                      <input type="text"  id="name" required/> 
+                    </div>
                      <br /><br />
 
+                     <div className="container">
                      <label htmlFor="mail">Email:</label>
                      <input type="email" id="mail" required />
+                     </div>
                      <br /><br />
 
+                     <div className="container">
                      <label htmlFor="phone">Phone:</label>
                      <input type="phone" id="phone" required />
+                     </div>
                      <br /><br />
                      
+                     <div className="container">
                      <label htmlFor="message">Message:</label>
-                     <textarea name="mesage" id="message" cols={50} rows={10}></textarea>
+                     <textarea name="mesage"  cols={50} rows={10}></textarea>
+                     </div>
                      <br /><br />
 
                      <div style={{textAlign: "center"}}>
